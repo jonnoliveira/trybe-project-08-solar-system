ceba2cb91c78186a36d98457d1c3afa12c267d86
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-10-21 16:42:28 -0300

    Requisito 10 - completo

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {missions
+          .map((mission, index) => {
+            const { name, year, country, destination } = mission;
+            return (
+              <MissionCard
+                key={ index }
+                name={ name }
+                year={ year }
+                country={ country }
+                destination={ destination }
+              />
+            );
+          })}
       </div>
     );
   }
