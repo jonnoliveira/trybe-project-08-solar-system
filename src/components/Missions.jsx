c6b9e1ec50c5193fa7e9ce1af5124dc9876945a4
Jonathas Assis de Oliveira <jonnutri@gmail.com>
2022-10-21 16:01:47 -0300

    Requisito 8 - completo - Missions with Title propst

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 class Missions extends Component {
   render() {
     return (
       <div data-testid="missions">
-        Missions
+        <Title headline="Missões" />
       </div>
     );
   }
